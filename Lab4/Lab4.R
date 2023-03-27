8c6e05a763d691001ba5ec6a32350e1c0215b4e6
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-03-27 20:52:57 +0300

    add report + fix

--- Lab4/Lab4.R
+++ Lab4/Lab4.R
@@ -14,7 +14,7 @@ first_m <- df_m[,c(1:2)][df_m$X1 > 0, ]
 first_f <- df_f[,c(1:2)][df_f$X1 > 0, ]
 
 pie(first_m$X1, labels=first_m$X1, col=rainbow(length(first_m$X1)), main = "Количество золотых медалей (мужчины)\nза все время")
-legend(-1, 1.5, first_m$Год, cex = 0.7, fill=rainbow(length(first_m$Год)))
+legend(-1.1, 1.1, first_m$Год, cex = 0.7, fill=rainbow(length(first_m$Год)))
 
 pie(first_f$X1, labels=first_f$X1, col=rainbow(length(first_f$X1)), main = "Количество золотых медалей (женщины)\nза все время")
 legend(-1, 1, first_f$Год, cex = 0.7, fill=rainbow(length(first_f$Год)))
@@ -41,7 +41,7 @@ lines(events_gold$Год, events_gold$Австралия, type="o", pch=19, col=
 lines(events_gold$Год, events_gold$Нидерланды, type="o", pch=19, col="brown")
 
 axis(side=1, at=events_gold$Год)
-legend(max(events_gold$Год) - 2, 53, c("США", "Китай", "Япония", "Великобритания", "Россия", "Австралия", "Нидерланды"), fill=c("#3be8b0", "#1aafd0", "#6a67ce", "#ffb900", "orchid", "#2e3c54", "brown"))
+legend(max(events_gold$Год) - 1.5, 53, c("США", "Китай", "Япония", "Великобритания", "Россия", "Австралия", "Нидерланды"), fill=c("#3be8b0", "#1aafd0", "#6a67ce", "#ffb900", "gray70", "#2e3c54", "brown"))
 
 
 
@@ -56,7 +56,7 @@ lines(events_prizes$Год, events_prizes$Австралия, type="o", pch=19, 
 lines(events_prizes$Год, events_prizes$Нидерланды, type="o", pch=19, col="brown")
 
 axis(side=1, at=events_prizes$Год)
-legend(max(events_prizes$Год) - 1, 137, c("США", "Китай", "Япония", "Великобритания", "Россия", "Австралия", "Нидерланды"), fill=c("#3be8b0", "#1aafd0", "#6a67ce", "#ffb900", "orchid", "#2e3c54", "brown"))
+legend(max(events_prizes$Год) - 1, 137, c("США", "Китай", "Япония", "Великобритания", "Россия", "Австралия", "Нидерланды"), fill=c("#3be8b0", "#1aafd0", "#6a67ce", "#ffb900", "gray70", "#2e3c54", "brown"))
 
 
 prize_6_m <- tail(prize_m, 6)
@@ -65,12 +65,12 @@ prize_6_f <- tail(prize_f, 6)
 par(mfrow=c(1,3))
 plot(prize_6_m, type="b", pch=19, col="navyblue", xaxt="n", ylim=c(0,7), main="Призовые места России по вольной борьбе\nза последние 6 ОИ")
 lines(prize_6_f, type="o", pch=11, col="hotpink")
-legend(min(prize_6_f$Год), 7.2, cex=0.7 ,c("Мужчины", "Женщины"), fill=c("navyblue", "hotpink","darkolivegreen3"))
+legend(min(prize_6_f$Год), 7.2, cex=0.7 ,c("Мужчины", "Женщины"), fill=c("navyblue", "hotpink"))
 axis(side=1, at=prize_m$Год)
 
 prize_grouped = data.frame(Призовых_М=prize_6_m$Призовых, Призовых_Ж=prize_6_f$Призовых)
-barplot(height=t(as.matrix(prize_grouped)), beside=TRUE, xlab="Год", ylab="Количество", names.arg=prize_6_f$Год, col=c("navyblue", "hotpink","darkolivegreen3"), main="Количество призовых мест России\nпо вольной борьбе за последние 6 ОИ")
+barplot(height=t(as.matrix(prize_grouped)), beside=TRUE, xlab="Год", ylab="Количество", names.arg=prize_6_f$Год, col=c("navyblue", "hotpink"), main="Количество призовых мест России\nпо вольной борьбе за последние 6 ОИ")
 
 prize_6_sum <- sapply(prize_grouped, sum)
-pie(prize_6_sum, labels=c(prize_6_sum["Призовых_М"], prize_6_sum["Призовых_Ж"]), col=c("navyblue", "hotpink","darkolivegreen3"), main="Всего призовых мест у М и Ж из России\nпо вольной борьбе за последние 6 ОИ")
+pie(prize_6_sum, labels=c(prize_6_sum["Призовых_М"], prize_6_sum["Призовых_Ж"]), col=c("navyblue", "hotpink"), main="Всего призовых мест у М и Ж из России\nпо вольной борьбе за последние 6 ОИ")
 
